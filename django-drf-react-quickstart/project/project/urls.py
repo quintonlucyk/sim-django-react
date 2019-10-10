27e1a027ec213afe1f00e2487293636e2c48a609
quintonlucyk <quinton_lucyk@yahoo.com>
2019-10-10 10:58:19 -0400

    left, react

--- django-drf-react-quickstart/project/project/urls.py
+++ django-drf-react-quickstart/project/project/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
 
-urlpatterns = [
-    path('', include('leads.urls')),
-]
+urlpatterns = [path("", include("leads.urls")), path("", include("frontend.urls"))]
+
